9bcef96d3c3bf5a36e942866e7dea1ba491d4c77
Crowdbotics <team@crowdbotics.com>
2020-06-08 06:54:20 +0000

    Update models

--- users/migrations/0003_wgwetgrgtrt.py
+++ users/migrations/0003_wgwetgrgtrt.py
@@ -6,15 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0002_auto_20200608_0640'),
+        ("users", "0002_auto_20200608_0640"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='WGWEtgrgtrt',
+            name="WGWEtgrgtrt",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('sww', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("sww", models.BigIntegerField()),
             ],
         ),
     ]
